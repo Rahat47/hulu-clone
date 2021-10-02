e1b0a8b570010f7064a7812319fb5f8ebb01169d
Rahat47 <djrayhan8@gmail.com>
2021-10-02 12:10:11 +0600

    basic homepage with next.js done

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,8 +1,13 @@
-const Results = () => {
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
+
+const Results = ({ results }) => {
     return (
-        <div>
-            <h1>Results</h1>
-        </div>
+        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center gap-1">
+            {results.map(result => (
+                <Thumbnail key={result.id} result={result} />
+            ))}
+        </FlipMove>
     );
 };
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,9 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
     return (
         <div>
             <Head>
@@ -19,7 +20,23 @@ export default function Home() {
             <Nav />
 
             {/* Results */}
-            <Results />
+            <Results results={results} />
         </div>
     );
 }
+
+export const getServerSideProps = async context => {
+    const genre = context.query.genre;
+    const request = await fetch(
+        `https://api.themoviedb.org/3${
+            requests[genre]?.url || requests.fetchTrending.url
+        }`
+    );
+    const data = await request.json();
+
+    return {
+        props: {
+            results: data?.results || null,
+        },
+    };
+};
